07c4a6f935885359afb7722d5865f42cec248d0e
gastyft <gastyftacla1@hotmail.com>
2024-11-02 15:09:30 -0300

    folders alumno y profesor

---
+++ src/app/componentes/profesor/crear-profesor/crear-profesor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-profesor',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-profesor.component.html',
+  styleUrl: './crear-profesor.component.css'
+})
+export class CrearProfesorComponent {
+
+}
