e929892c48dbff13911946eec3fd216e4804a931
gastyft <gastyftacla1@hotmail.com>
2024-11-03 07:54:34 -0300

    creacion de algunos componentes de alumno

--- src/app/componentes/alumno/nav-alumno/nav-alumno.component.spec.ts
+++ src/app/componentes/alumno/nav-alumno/nav-alumno.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { NavAlumnoComponent } from './nav-alumno.component';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+describe('NavAlumnoComponent', () => {
+  let component: NavAlumnoComponent;
+  let fixture: ComponentFixture<NavAlumnoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [NavAlumnoComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(NavAlumnoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/componentes/alumno/nav-alumno/nav-alumno.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-alumno',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-alumno.component.html',
+  styleUrl: './nav-alumno.component.css'
+})
+export class NavAlumnoComponent {
+
+}
